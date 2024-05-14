fdb6d5697a18b862a86bec681b7e653db0c8bad0
Juan Chuc <jachucme@gmail.com>
2024-05-13 21:12:11 -0600

    UPD: Se modifica Main para eliminar la solicitud de argumentos para crear un nuevo usuario

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,24 +5,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        for (String arg: args) {
-            System.out.println(arg);
+        UserDAO userDao = new UserDAO();
+        User user = new User("Pepito", "Conejo", "pepitoconejo@gmail.com");
+
+        boolean usuarioCreado = userDao.agregar(user);
+
+        if (usuarioCreado) {
+            System.out.println("El usuario ha sido creado");
+        } else {
+            System.err.println("Hubo un problema al crear al usuario");
         }
 
-        if (args.length > 0) {
-            UserDAO userDao = new UserDAO();
-            User user = new User(args[0], args[1], args[2]);
-
-            boolean usuarioCreado = userDao.agregar(user);
-
-            if (usuarioCreado) {
-                System.out.println("El usuario ha sido creado");
-            } else {
-                System.err.println("Hubo un problema al crear al usuario");
-            }
-
-            ArrayList<User> usuarios = userDao.consultar();
-            System.out.println(usuarios.toString());
-        }
+        ArrayList<User> usuarios = userDao.consultar();
+        System.out.println(usuarios.toString());
     }
 }
